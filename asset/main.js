e37a6d7513b3f621d381a13748a1a22042f48b03
Bui Huu Dat <dat54261001@gmail.com>
2021-10-06 09:01:10 +0700

    update chart

--- asset/main.js
+++ asset/main.js
@@ -17,3 +17,63 @@ function activeLink(){
 }
 list.forEach((item) => 
     item.addEventListener('mouseover', activeLink));
+
+
+// pie
+var ctx = document.getElementById('myChartPolarArea').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ['Pending', 'Delivered', 'In Progress'],
+        datasets: [{
+            title: {
+                display: true,
+                text: "Status"
+            },
+            label: '# of Votes',
+            data: [1200, 1900, 3000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+        }]
+    },
+    options: {
+        options: {
+            responsive: true,
+        }
+    }
+});
+// bar
+var ctx = document.getElementById('myChartBar').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
